3d3460241433729581c4996418474ae3863257bd
Misash <misash@outlook.com>
2021-11-19 12:14:51 -0500

    Rest-API -Employees

---
+++ dataBase/db.sql
@@ -0,0 +1,53 @@
+
+
+
+CREATE DATABASE IF NOT EXISTS company ;
+
+USE company;
+
+CREATE TABLE  employees(
+	id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) DEFAULT NULL,
+    salary INT(11) DEFAULT NULL ,
+    PRIMARY KEY(id)
+);
+
+DESCRIBE employees;
+
+INSERT INTO employees VALUES
+	(1,'Ryan Ray',20000),
+    (2,'Joe McMillan',40000),
+    (3,'Jhon Carter',50000);
+
+SELECT * FROM employees;
+
+--PROCEDURES 
+
+
+
+USE company;
+
+DELIMITER $$
+USE `company`$$
+
+CREATE PROCEDURE `employeeAddOrEdit` (
+  IN _id INT,
+  IN _name VARCHAR(45),
+  IN _salary INT
+)
+BEGIN 
+  IF _id = 0 THEN
+    INSERT INTO employees (name, salary)
+    VALUES (_name, _salary);
+
+    SET _id = LAST_INSERT_ID();
+  ELSE
+    UPDATE employees
+    SET
+    name = _name,
+    salary = _salary
+    WHERE id = _id;
+  END IF;
+
+  SELECT _id AS 'id';
+END
